312898e16bb42b8696654283900840f24efaf2c9
jcmorales50 <jcmorales50@hotmail.com>
2021-04-08 17:38:08 +0200

    final version

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;    
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate/100;
+    var today = new Date();
+    var year = parseInt(today.getFullYear()) + parseInt(years);
+
+    if (principal !="") {
+        document.getElementById("result").innerHTML = '<hr>If you deposit <mark>' + principal + '</mark>,<br>at an interest rate of <mark>' + rate + '%</mark>.<br> You will receive an amount of <mark>' + interest + '</mark>,<br> in the year <mark>' + year + '</mark><hr><br>';        
+  
+    } else alert("Enter the principal amount");
+
+}
+
+function slider_update()
+{
+    var rate = document.getElementById("rate");
+    var rate_text = document.getElementById("rate_text");
+
+    var valor = parseFloat(rate.value).toFixed(2);
+    if (valor < 10) {
+        valor = "0" + valor;
+    }
+
+    rate_text.innerHTML = valor +"%"; 
+}
+
+function clean ()
+{
+    document.getElementById("result").innerHTML = "";
 }
         
